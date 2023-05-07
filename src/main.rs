a4d70935b0f543b33b454dd73b4c9b706d05731d
MehrCodeLand <mehrcodeland@gmail.com>
2023-05-07 14:03:16 +0330

    hello_world

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // 1 
+    let num1 = 1 ;
+    let num2 = 3 ;
+    let sum = num1 + num2 ;
+    println!("{}" , sum);
+
+    // 2
+    let age = 20 ;
+    // error age = 21 ; 
+
+    let mut age2 = 23 ;
+    println!("{}", age2);
+    age2 = 30 ;
+    println!("{}", age2);
+
+    const PI:f64 = 3.14 ;
+    println!("{}", PI);
+
+    // 3
+}
